f43e3fc3d93e20c18edc70acc240758c4f9985b1
Nikola Stojanovic <nikola.stojanovic@codecentric.de>
2021-07-24 15:10:40 +0200

    Add needle docs

--- Onboarding/OnboardingTestApp/SceneDelegate.swift
+++ Onboarding/OnboardingTestApp/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Onboarding
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -16,7 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+
+        let splashDuration: TimeInterval = 2 // instead of hardcoding a magic number, provide value from the design system
+        let bundle: Bundle = .main // instead of .main provide bundle of design system since in it resources will be placed
+
+        let root = Onboarding.bootstrap(splashDuration: splashDuration, bundle: bundle)
+        window?.rootViewController = root.splashViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
